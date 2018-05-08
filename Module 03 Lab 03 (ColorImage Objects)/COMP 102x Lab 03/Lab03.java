f7304925b52f69ca73459745c5b7719ed150c0d7
Pulkit219 <ascension1110@gmail.com>
2018-05-08 16:09:55 -0400

    5th commit
    Lab 03 Graded Task 3 - Image Addition

--- Module 03 Lab 03 (ColorImage Objects)/COMP 102x Lab 03/Lab03.java
+++ Module 03 Lab 03 (ColorImage Objects)/COMP 102x Lab 03/Lab03.java
@@ -42,8 +42,17 @@ public class Lab03
     public void imageAddition() 
     {    
         // Please write your code after this line
+        ColorImage imageA = new ColorImage();
+        ColorImage imageB = new ColorImage();
+        ColorImage imageC = imageA.add(imageB);
         
+        //Setting up canvas according to image height and width
+        Canvas canvas = new Canvas(imageA.getWidth()*3+20, imageA.getHeight());
         
+        //  load image to canvas
+        canvas.add(imageA,0,0);
+        canvas.add(imageB,imageA.getWidth()+10,0);
+        canvas.add(imageC,imageB.getWidth()*2+20,0);
         
         
     }
